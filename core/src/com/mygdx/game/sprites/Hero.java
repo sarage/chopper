0e5694f3c140746a897e7265d410d21d20b5161b
zako16 <pojiratelnica@gmail.com>
2017-05-16 14:07:50 +0600

    turn to left, turn to right

--- core/src/com/mygdx/game/sprites/Hero.java
+++ core/src/com/mygdx/game/sprites/Hero.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.TextureData;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.environment.AmbientCubemap;
 import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.ArrowShapeBuilder;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 
@@ -21,14 +22,17 @@ public class Hero {
     private Vector3 velosity;
     private Texture texture;
     private Animation heroAnimation;
-
+    private Rectangle bound;
+    private int state; //1-stay; 2-turn left; 3-turn right; 4-left go; 5- right go;
 
 
     public Hero(int x, int y){
+        state = 1;
         position = new Vector3(x,y,0);
         velosity = new Vector3(0,0,0);
-        texture = new Texture("left_go.png");
-        heroAnimation = new Animation(new TextureRegion(texture), 6, 1f);
+        texture = new Texture("stay.png");
+        heroAnimation = new Animation(new TextureRegion(texture), 3, 1f);
+        bound = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
     }
 
     public Vector3 getPosition() {
@@ -55,13 +59,48 @@ public class Hero {
         if (position.x>1900)
             position.x=1900;
         heroAnimation.updates(dt);
+        bound.setPosition(position.x, position.y);
     }
 
-    public void Walking(){
+    public void stay(){
+        this.state=1;
+        this.texture = new Texture("stay.png");
+        this.heroAnimation = new Animation(new TextureRegion(texture), 3, 1f);
+    }
+
+    public void turnleft(){
+        this.state=2;
+        this.texture = new Texture("left.png");
+        this.heroAnimation = new Animation(new TextureRegion(texture), 4, 1f);
+    }
+
+    public void turnright(){
+        this.state=3;
+        this.texture = new Texture("right.png");
+        this.heroAnimation = new Animation(new TextureRegion(texture), 4, 1f);
+    }
+
+    public void walkleft(){
+        state = 4;
+        texture = new Texture("left_go.png");
+        heroAnimation = new Animation(new TextureRegion(texture), 6, 1f);
         velosity.x=1920;
+    }
+
+//    public void wa
+
+    public int getX(){
+        return (int)this.position.x;
+    }
+    public int getState(){
+        return this.state;
     }
 
     public void dispose(){
         texture.dispose();
+    }
+
+    public Rectangle getBound(){
+        return bound;
     }
 }
--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -20,25 +20,52 @@ public class PlayState extends State{
     private Flasks flask2;
     private Flasks flask3;
     private Flasks flask4;
+    private int flasksCount;
 
 
     public PlayState(GameStateManagment gsm) {
         super(gsm);
         camera.setToOrtho(false, MyGdxGame.WIDTH,MyGdxGame.HEIGHT);
-        hero = new Hero(0,10);
+        hero = new Hero(MyGdxGame.WIDTH/3,10);
         flask1 = new Flasks("flask2.png", 270,195);
         flask2 = new Flasks("flask1.png",283,330);
         flask3 = new Flasks("flask4.png",630,450);
         flask4 = new Flasks("flask3.png",500,100);
+        flasksCount = 0;
         background = new Texture("wall.png");
     }
 
     @Override
     protected void handleInput() {
+        // 1-stay; 2-turn left; 3-turn right; 4-left go; 5- right go;
         if (Gdx.input.justTouched()){
-            hero.Walking();
+            if (hero.getX() < Gdx.input.getX() && hero.getState() == 1){
+                // если heroX меньше чем touchX и позиция прямо то надо повернуть влево
+                hero.turnleft();
+            }
+//            -если heroX меньше чем touchX и позиция влево то надо пойти влево
+//            else if(hero.getX() < Gdx.input.getX() && hero.getState() == 2){
+//                hero.walkleft();
+//            }
+            else if(hero.getX() > Gdx.input.getX() && hero.getState() == 2 || hero.getState() == 4){
+                // если heroX больше чем touchX и позиция пойти влево или влево то надо прямо
+                hero.stay();
+            }
+            else if (hero.getX() > Gdx.input.getX() && hero.getState() == 1){
+                // если heroX больше чем touchX и позиция прямо то надо повернуть вправо
+                hero.turnright();
+            }
+//            -если heroX больше чем touchX и позиция вправо то пойти вправо
+//            else if (hero.getX() > Gdx.input.getX() && hero.getState() == 3){
+//                hero.walkright();
+//            }
+            else if (hero.getX() < Gdx.input.getX() && hero.getState() == 3 || hero.getState()==5){
+                // если heroX меньше чем touchX и позиция право или пойти вправо то прямо
+                hero.stay();
+            }
         }
     }
+
 
     @Override
     public void update(float dt) {
@@ -48,6 +75,25 @@ public class PlayState extends State{
         flask2.update(dt);
         flask3.update(dt);
         flask4.update(dt);
+        if (flask1.collite(hero.getBound())){
+//            flask1.remove();
+            flasksCount = flasksCount + 1;
+        }
+//        if (flask2.collite(hero.getBound())){
+//            flask2.remove();
+//            flasksCount = flasksCount + 1;
+//        }
+//        if (flask3.collite(hero.getBound())){
+//            flask3.remove();
+//            flasksCount = flasksCount + 1;
+//        }
+//        if (flask4.collite(hero.getBound())){
+//            flask4.remove();
+//            flasksCount = flasksCount + 1;
+//        }
+//        if (flasksCount == 3){
+//            Gdx.app.exit();
+//        }
     }
 
     @Override
